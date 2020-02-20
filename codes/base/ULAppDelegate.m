68e212add6728f5a29a6ee4b23d1ebeddf5ab474
ulmac01 <ulmac01@3gwin.cn>
2020-02-20 11:32:02 +0800

    用户选择网络权限选项后再进入后续启动流程

--- codes/base/ULAppDelegate.m
+++ codes/base/ULAppDelegate.m
@@ -12,6 +12,7 @@
 #import "ULNotification.h"
 #import "ULLogoViewController.h"
 #import "ULSDKManager.h"
+#import "ZYNetworkAccessibity.h"
 
 @implementation ULAppDelegate
 
@@ -25,14 +26,37 @@
 + (void)startSDK:(NSNotification*) notification{
     NSLog(@"%s",__func__);
     
-    [ULConfig initConfigInfo];
+    //网络权限处理
+    //网络判断
+    //    [ZYNetworkAccessibity setAlertEnable:YES];
     
-    [ULCop initCopInfo];
+    [ZYNetworkAccessibity setStateDidUpdateNotifier:^(ZYNetworkAccessibleState state) {
+        //ZYNetworkChecking
+        //ZYNetworkUnknown
+        //ZYNetworkAccessible 选择无限网和蜂窝时
+        //ZYNetworkRestricted 弹出权限申请框or选择不允许or仅无限局域网
+        NSString *desc = @"用户设置的不允许或仅限于无线网络";
+        if(state == ZYNetworkAccessible){
+            desc = @"当前应用网络权限可用";
+        }
+        NSLog(@"%s%@", __func__ ,desc);
+        //1.获取应用网络权限 TODO 用户无论选择哪个选项都会进游戏
+        //2.判断当前网络状况，进行无网提示 TODO
+        
+        [ULConfig initConfigInfo];
+        
+        [ULCop initCopInfo];
+        
+        //在启动viewController相关之前先初始化模块。TODO 提前初始化的目的只有一个，那就是确保applicationDidBecomeActive生命周期能被正常监听
+        [ULSDKManager init];
+        
+        [self startLogoView];
+            
+    }];
+        
+    [ZYNetworkAccessibity start:[[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleIdentifier"]];
     
-    //在启动viewController相关之前先初始化模块。TODO 提前初始化的目的只有一个，那就是确保applicationDidBecomeActive生命周期能被正常监听
-    [ULSDKManager init];
     
-    [self startLogoView];
 }
 
 
