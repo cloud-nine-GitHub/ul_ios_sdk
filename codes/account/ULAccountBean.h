fc77673d7500c9ca3572893cf318cae4f268bc17
ulmac01 <ulmac01@3gwin.cn>
2020-02-22 12:21:09 +0800

    添加统计上报数据实体类

---
+++ codes/account/ULAccountBean.h
@@ -0,0 +1,32 @@
+//
+//  ULAccountBean.h
+//  ulsdkgamedemo
+//
+//  Created by 一号机雷兽 on 2020/2/22.
+//  Copyright © 2020 一号机雷兽. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+/**
+ 
+ 将每条数据封装为一个对象
+ 
+ */
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ULAccountBean : NSObject
+{
+    NSString *_upData;  //数据
+    int _upDataId; //排序ID
+    
+}
+
+
+@property (nonatomic,strong) NSString *upData;
+@property (nonatomic,assign) int upDataId;
+
+
+-(id)initWithId:(int )upDataId andUpData:(NSString *)upData;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ codes/account/ULAccountBean.m
@@ -0,0 +1,26 @@
+//
+//  ULAccountBean.m
+//  ulsdkgamedemo
+//
+//  Created by 一号机雷兽 on 2020/2/22.
+//  Copyright © 2020 一号机雷兽. All rights reserved.
+//
+
+#import "ULAccountBean.h"
+
+@implementation ULAccountBean
+
+
+-(id)initWithId:(int )upDataId andUpData:(NSString *)upData
+{
+    
+    if (self = [super init]) {
+        _upDataId = upDataId;
+        _upData = upData;
+    }
+    return self;
+    
+}
+
+
+@end
