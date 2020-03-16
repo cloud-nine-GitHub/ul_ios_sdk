c7ef85ff1123b8b4520ad687216f016a8c7887fe
ulmac01 <ulmac01@3gwin.cn>
2020-03-16 19:30:56 +0800

    遗漏原生缓存头文件提交

---
+++ codes/base/ULNativeAdvItemCacher.h
@@ -0,0 +1,19 @@
+//
+//  ULNativeAdvItemCacher.h
+//  ulsdkgamedemo
+//
+//  Created by 一号机雷兽 on 2020/3/16.
+//  Copyright © 2020 一号机雷兽. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ULNativeAdvItemCacher : NSObject
+
+
+//-(id)initWithProvider:()
+@end
+
+NS_ASSUME_NONNULL_END
